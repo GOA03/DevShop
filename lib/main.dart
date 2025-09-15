b8284281b4fd824a29ac08c77776e9e53d748a70
guiopauer <guiopauer@hotmail.com>
2025-09-15 19:48:39 -0300

    refactor: rename app title from ShopEasy to DevShop

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ShopEasy',
+      title: 'DevShop',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
